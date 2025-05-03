e9e22b61fc2b7d0a276ee0a6d46e4a7dce7d9013
johnhhood <johnhood2013@gmail.com>
2025-05-03 01:01:00 -0500

    Update main.jsx
    add toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { SessionProvider } from "./lib/SessionContext";
 import "./index.css";
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SessionProvider>  
       <BrowserRouter>
+        <Toaster position="top-right" />
         <App />
       </BrowserRouter>
     </SessionProvider>

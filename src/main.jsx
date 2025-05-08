c7627dcf901ffb018ebcb54bb2b0dc05a938d01e
johnhhood <johnhood2013@gmail.com>
2025-05-08 11:38:13 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { SessionProvider } from "./lib/SessionContext";
 import "./index.css";
-import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SessionProvider>  
-      <BrowserRouter>
-        <Toaster position="top-right" />
-        <App />
-      </BrowserRouter>
-    </SessionProvider>
+    <App />
   </React.StrictMode>
 );

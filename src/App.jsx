33e6e6b5751166eb053f0056cbc6716d8342a5b1
johnhhood <johnhood2013@gmail.com>
2025-05-08 11:48:41 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
+import Login from './pages/Login';  // Add this line
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path="/login" element={<Login />} />  {/* Add this route */}
       </Routes>
     </Router>
   );
